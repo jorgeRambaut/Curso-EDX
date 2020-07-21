a2df4cecd136137233af9ed7856b7ef2392e2adc
Jorge <jgrambaut@gmail.com>
2020-07-21 20:00:34 -0300

    agrego nuevos files y html

--- application.py
+++ application.py
@@ -1,13 +1,61 @@
 """ importamos Flask"""
-from flask import Flask
+from flask import Flask, render_template, request, session
+"""from flask_session import Session"""
+
+
 """ nueva app """
 app = Flask(__name__)
-"""default page"""
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+"""Session(app)"""
+
+notes=[]
+
+@app.route("/", methods=["GET", "POST"])
+def notas():
+    if request.method== "POST":
+        note = request.form.get("note")
+        notes.append(note)
+
+    return render_template("notas.html",notes=notes)
+
+
+
+"""@app.route("/")
+def index():
+    return render_template("Prueba.html")
+
+@app.route("/hello",methods=["POST"])
+def hello():
+    name= request.form.get("name")
+    return render_template("hello.html", name=name)
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
+
+@app.route("/")
+def Prueba():
+    names=["Alice","Bob","Charlie"]
+    return render_template("Prueba.html",names=names)
+
+default page
 @app.route("/")
 def index():
-    return "hello,world!"
+    headline="hola, paso headline por python"
+    return render_template("index.html",headline=headline)
 
-@app.route("/<string:name>")
-def hello(name):
-    name = name.capitalize()
-    return f"<h1>hola,{name}!<h1>"
+
+@app.route("/bye")
+def bye():
+    headline="paso headline por python, adios"
+    return render_template("index.html",headline=headline)
+
+
+@app.route("/")
+
+def index():
+    now=datetime.datetime.now()
+    new_year=now.month==1 and now.day==1
+    return render_template("index.html",new_year=new_year)"""
