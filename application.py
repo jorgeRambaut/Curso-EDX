271660e5a9192698821a165a2a8f09c124c60f15
Jorge <jgrambaut@gmail.com>
2020-07-21 21:24:49 -0300

    agrego sesion particular back end

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 """ importamos Flask"""
 from flask import Flask, render_template, request, session
-"""from flask_session import Session"""
+from flask_session import Session
+
 
 
 """ nueva app """
@@ -8,17 +9,21 @@ app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-"""Session(app)"""
+Session(app)
 
 notes=[]
 
 @app.route("/", methods=["GET", "POST"])
 def notas():
+    """""sesiones particulares"""
+    if session.get("notes") is None:
+        session["notes"]=[]
     if request.method== "POST":
         note = request.form.get("note")
-        notes.append(note)
+        session["notes"].append(note)
+        """notes.append(note)"""
 
-    return render_template("notas.html",notes=notes)
+    return render_template("notas.html",notes=session["notes"])
 
 
 
